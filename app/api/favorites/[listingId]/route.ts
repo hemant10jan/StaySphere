742b3a8990ec1f29d328705f66bf60edde9d474d
Hemant Singh <hemant10jan@gmail.com>
2025-02-21 21:02:25 +0530

    Test3

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
 
@@ -6,7 +6,7 @@ interface IParams {
   listingId?: string;
 }
 
-export async function POST(request: Request, { params }: { params: IParams }) {
+export async function POST(request: NextRequest, { params }: { params: IParams }) {
   const currentUser = await getCurrentUser();
   if (!currentUser) return NextResponse.error();
   const { listingId } = params;
